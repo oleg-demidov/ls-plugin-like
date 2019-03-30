105ec83ae3b9b1243e1ce0c997f538a8357b46e5
olezhikz <boxmilo@gmail.com>
2019-03-30 18:16:28 +0600

    Dump

--- update/1.0.0/dump.sql
+++ update/1.0.0/dump.sql
@@ -1,9 +1,50 @@
-CREATE TABLE `prefix_wiki` (
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2.1
+-- http://www.phpmyadmin.net
+--
+-- Хост: localhost
+-- Время создания: Мар 30 2019 г., 12:15
+-- Версия сервера: 5.7.25-0ubuntu0.16.04.2
+-- Версия PHP: 5.6.40-1+ubuntu16.04.1+deb.sury.org+1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `pddfend`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `prefix_like`
+--
+
+CREATE TABLE `prefix_like` (
+  `target_id` int(10) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `entity_id` bigint(20) UNSIGNED NOT NULL,
+  `state` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `prefix_like_target`
+--
+
+CREATE TABLE `prefix_like_target` (
   `id` int(11) NOT NULL,
-  `code` varchar(50) COLLATE utf8_bin NOT NULL,
-  `menu_title` varchar(100) COLLATE utf8_bin NOT NULL,
-  `menu_enable` tinyint(1) NOT NULL DEFAULT '0',
-  `state` tinyint(1) NOT NULL DEFAULT '0'
+  `entity` varchar(100) COLLATE utf8_bin NOT NULL,
+  `code` varchar(10) COLLATE utf8_bin NOT NULL,
+  `title` varchar(200) COLLATE utf8_bin NOT NULL,
+  `date_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -11,85 +52,32 @@ CREATE TABLE `prefix_wiki` (
 --
 
 --
--- Индексы таблицы `prefix_wiki`
+-- Индексы таблицы `prefix_like`
 --
-ALTER TABLE `prefix_wiki`
+ALTER TABLE `prefix_like`
+  ADD KEY `target_id` (`target_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `entity_id` (`entity_id`),
+  ADD KEY `state` (`state`);
+
+--
+-- Индексы таблицы `prefix_like_target`
+--
+ALTER TABLE `prefix_like_target`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `code` (`code`);
-
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `prefix_wiki`
---
-ALTER TABLE `prefix_wiki`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-CREATE TABLE `prefix_wiki_page` (
-  `id` int(11) NOT NULL,
-  `wiki_id` int(11) NOT NULL,
-  `code` varchar(500) COLLATE utf8_bin NOT NULL,
-  `title` varchar(200) COLLATE utf8_bin DEFAULT NULL,
-  `description` varchar(200) COLLATE utf8_bin DEFAULT NULL,
-  `keywords` varchar(200) COLLATE utf8_bin DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Дамп данных таблицы `prefix_wiki_page`
---
-
-INSERT INTO `prefix_wiki_page` (`id`, `wiki_id`, `code`, `title`, `description`, `keywords`) VALUES
-(2, 1, '0', 'Знаки', 'sdfsdfsdfsd', 'fsdfsdfsdf');
-
---
--- Индексы сохранённых таблиц
---
-
---
--- Индексы таблицы `prefix_wiki_page`
---
-ALTER TABLE `prefix_wiki_page`
-  ADD PRIMARY KEY (`id`),
+  ADD KEY `entity` (`entity`),
   ADD KEY `code` (`code`),
-  ADD KEY `wiki_id` (`wiki_id`);
+  ADD KEY `date_create` (`date_create`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
 --
--- AUTO_INCREMENT для таблицы `prefix_wiki_page`
+-- AUTO_INCREMENT для таблицы `prefix_like_target`
 --
-ALTER TABLE `prefix_wiki_page`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
-
-CREATE TABLE `prefix_wiki_punkt` (
-  `id` int(11) NOT NULL,
-  `page_id` int(11) NOT NULL,
-  `name` varchar(50) COLLATE utf8_bin NOT NULL,
-  `text` varchar(2000) COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Индексы сохранённых таблиц
---
-
---
--- Индексы таблицы `prefix_wiki_punkt`
---
-ALTER TABLE `prefix_wiki_punkt`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `name` (`name`);
-
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `prefix_wiki_punkt`
---
-ALTER TABLE `prefix_wiki_punkt`
+ALTER TABLE `prefix_like_target`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
